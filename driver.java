ae8f1cb52830f31d3edafff8decf5b33493012a3
padraigmc <44905245+padraigmc@users.noreply.github.com>
2019-04-04 14:07:44 +0100

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,9 +1,33 @@
+import java.util.*;
+
 public class driver {    
-    public static void main(String[] args) {
-        ArrayList<String> word = new ArrayList<>();
+    public static void main(String[] args) { 
         
-        WordSearchPuzzle driver = new WordSearchPuzzle(word);
+        WordSearchPuzzle driver = new WordSearchPuzzle("wordFile.txt", 5, 5, 6);
         driver.generateWordSearchPuzzle();
-        System.out.println("\n" + driver.getPuzzleAsString());
+        System.out.println("\n~ P U Z Z L E ~");
+        System.out.println("Unused spaces filed and answers below.\n");
+        driver.showWordSearchPuzzle(false);
+
+
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("beef");
+        food.add("avocado");
+        food.add("water");
+        food.add("beans");
+        food.add("wheat");
+        food.add("flour");
+
+        WordSearchPuzzle ws = new WordSearchPuzzle(food);
+        ws.generateWordSearchPuzzle();
+        System.out.println("\n~ P U Z Z L E ~");
+        System.out.println("Unused spaces filed and answers hidden.\nThe words sorted in order of length:");
+        for (int i = 0; i < ws.getWordSearchList().size(); i++) {
+            System.out.println(ws.getWordSearchList().get(i));
+        }
+        String puzzle = ws.getPuzzleAsString();
+        System.out.println("\n\n" + puzzle);
+
     }
 }
